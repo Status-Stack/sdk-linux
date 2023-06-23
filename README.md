5b34b7f12e8c00fd367d156445bca331c1164141
Lukáš Hron <info@lukashron.cz>
2023-06-23 17:38:47 +0200

    Usage disk space

--- README.md
+++ README.md
@@ -14,11 +14,13 @@ Usage
   # Example CRON task "1 * * * * /path/to/monitor.sh > /dev/null"
   # (script must by executable: "sudo chmod +x monitor.sh")
   
-  $ sh ./monitor.sh [-l]
+  $ sh ./monitor.sh [-l] [-s <filesystem>]
 ```
 
 **Options**
 
 `-l` send load average data
+
+`-s <filesystem>` send space usage data
 
 Copyright (c) 2022-2023 Status Stack
--- monitor.sh
+++ monitor.sh
@@ -2,14 +2,21 @@
 
 . ./local.config
 
+#
+# Command usage info
+#
 help() {
   echo ""
-  echo "Usage: sh ./monitor.sh [-l]"
+  echo "Usage: sh ./monitor.sh [-ls]"
   echo ""
   echo "       -l          Load average"
+  echo "       -s          Space usage"
   echo ""
 }
 
+#
+# Send StatusStack request
+#
 send_request() {
   /usr/bin/curl --request POST \
      --url "${1}" \
@@ -19,6 +26,9 @@ send_request() {
      --data "{\"tag\":\"${3}\",\"data\":\"${4}\",\"priority\":0}"
 }
 
+#
+# Send load average data
+#
 send_load_average() {
   if [[ "$OSTYPE" == "darwin"* ]]; then
     value=$(uptime | awk -F "load averages: " '{print $2}' | awk -F " " '{gsub(",", "."); print $1}')
@@ -26,13 +36,33 @@ send_load_average() {
     value=$(uptime | awk -F "load average: " '{print $2}' | awk -F "," '{print $1}')
   fi
 
+  echo ""
   echo "Load average data send"
   send_request "${1}" "${2}" "load-averages" "${value}"
 }
 
-while getopts ":l" args; do
+#
+# Send usage disk space
+#
+send_usage_space() {
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    value=$(df | awk -v filter="${3//\//\/}" '$0~filter{gsub("%", ""); print $5}')
+  else
+    value=$(df ${3} | awk '{gsub("%", ""); print $5}')
+  fi
+
+  echo ""
+  echo "Space usage info send"
+  send_request "${1}" "${2}" "usage-space" "${value}"
+}
+
+#
+# Metrics switcher
+#
+while getopts ":ls:" args; do
   case "${args}" in
     l) send_load_average "${API_ENDPOINT}" "${API_KEY}" ;;
+    s) send_usage_space "${API_ENDPOINT}" "${API_KEY}" "${OPTARG}" ;;
     \?) echo "Invalid option -$OPTARG" >&2; help; exit 1 ;;
   esac
 done
